8530760e0077ad3323df676d8ab6729c1e4e6781
Stephane Duverney <68503102+wyshiko@users.noreply.github.com>
2022-09-20 22:30:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-
-read -p "intialisation du .zshrc? (Y/N): "  confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-echo "initialisation du .zshrc"
-cp zshrc ~/.zshrc
-echo "initialisation du .p10.zsh"
-cp p10k.zsh ~/.p10k.zsh
+echo "           __ __                     ";
+echo "   __  __ / // /_ _____ ____   ____  ";
+echo "  / / / // // __// ___// __ \ / __ \ ";
+echo " / /_/ // // /_ / /   / /_/ // / / / ";
+echo " \__,_//_/ \__//_/    \____//_/ /_/  ";
+echo "              setup                  ";
+echo "                                     ";
+read -p "Do you want install ultron and customiz your shell (the ~/.zshrc file will me edit) (Y/N): "  confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+then
+    # Past .zshrc custom file to ~/ folder  
+    echo "initialisation du .zshrc"
+    cp zshrc ~/.zshrc
+    sudo apt install zsh
+fi
+read -p "Do you want set .p10k.zsh file in your ~ folfer (Y/N): "  confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+then
+    # Past .p10k.zshcustom file to ~/ folder  
+    echo "initialisation du .p10k.zsh"
+    cp p10k.zsh ~/.p10k.zsh
+fi
+# Install antigen pluggin manager
 cd ~
-sudo apt install zsh
 curl -L git.io/antigen > antigen.zsh
+read -p "Do you want install Kali's tools (Y/N): "  confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+then
+    git clone https://github.com/LionSec/katoolin.git && cp katoolin/katoolin.
+    chmod +x /usr/bin/katoolin
+    sudo katoolin
+fi
+echo'THANK YOU'
 zsh
