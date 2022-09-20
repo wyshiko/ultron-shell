ef2a1eee672aadd0cb6560452e8378c306c3281e
Stephane Duverney <68503102+wyshiko@users.noreply.github.com>
2022-09-21 00:06:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,17 +36,25 @@ then
     ## Launch the container
     lxc launch images:kali/current/amd64 my-kali
 
-    ## Install some additional applications, use either light or default
     lxc exec my-kali -- passwd                         ## First things first
     lxc exec my-kali -- apt install kali-linux-light   ## Bare minimum
-    lxc exec my-kali -- apt install kali-linux-default ## Default set of packages
 
-    ## Setup non-root user
-    lxc exec my-kali -- adduser kali
-    lxc exec my-kali -- usermod -aG sudo kali
-    lxc exec my-kali -- sed -i '1 i\TERM=xterm-256color' /home/kali/.bashrc
-    lxc exec my-kali -- sh -c "echo 'Set disable_coredump false' > /etc/sudo.conf"
-    echo lxc console my-kali > how_to_launch_kali.txt
+    read -p "Do you want install a Kali packages (Y/N): "  confirm
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+    then
+        ## Install some additional applications, use either light or default
+        lxc exec my-kali -- apt install kali-linux-default ## Default set of packages
+    fi
+    read -p "Do you want create an non-root user (Y/N): "  confirm
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+    then
+        ## Setup non-root user
+        lxc exec my-kali -- adduser kali
+        lxc exec my-kali -- usermod -aG sudo kali
+        lxc exec my-kali -- sed -i '1 i\TERM=xterm-256color' /home/kali/.bashrc
+        lxc exec my-kali -- sh -c "echo 'Set disable_coredump false' > /etc/sudo.conf"
+        echo lxc console my-kali > how_to_launch_kali.txt
+    fi
 fi
 echo ''
 echo 'INSTALLATION FINISH, THANK YOU'
