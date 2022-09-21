3241cac06608c5a485805945b1565716cd84ba75
Stephane Duverney <68503102+wyshiko@users.noreply.github.com>
2022-09-21 19:06:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,37 +25,6 @@ fi
 # Install antigen pluggin manager
 cd ~
 curl -L git.io/antigen > antigen.zsh
-read -p "Do you want install a Kali Linux LXD Image (Y/N): "  confirm
-if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
-then
-    cd ~
-    ## Install LXD
-    sudo snap install lxd
-    lxd init
-
-    ## Launch the container
-    lxc launch images:kali/current/arm64 my-kali
-    echo lxc console my-kali > how_to_launch_kali.txt
-
-    lxc exec my-kali -- passwd                         ## First things first
-    lxc exec my-kali -- apt install kali-linux-light   ## Bare minimum
-
-    read -p "Do you want install a Kali packages (Y/N): "  confirm
-    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
-    then
-        ## Install some additional applications, use either light or default
-        lxc exec my-kali -- apt install kali-linux-default ## Default set of packages
-    fi
-    read -p "Do you want create an non-root user (Y/N): "  confirm
-    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
-    then
-        ## Setup non-root user
-        lxc exec my-kali -- adduser kali
-        lxc exec my-kali -- usermod -aG sudo kali
-        lxc exec my-kali -- sed -i '1 i\TERM=xterm-256color' /home/kali/.bashrc
-        lxc exec my-kali -- sh -c "echo 'Set disable_coredump false' > /etc/sudo.conf"
-    fi
-fi
 echo ''
 echo 'INSTALLATION FINISH, THANK YOU'
 echo ''
